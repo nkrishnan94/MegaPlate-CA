39b0ed58971a78adcc4417f573f0e29d20f9d6e7
nkrishnan94 <npk13@case.edu>
2019-08-15 10:20:52 -0400

    Run faster
    only iterate trough cells with empty neighbors

--- funcs.py
+++ funcs.py
@@ -18,84 +18,84 @@ def fitness_func(d,m,k,s_p,s,pk):
     y = s + (p)* ((d**k)/((d**k)- (s-p)/s))*((m - pk)/pk)
     return y
 
-def megaplate_sim(length,width, divs,abx,mut_rate, k,s_p,s,pk):
+def megaplate_sim(length ,width, divs,abx,mut_rate, k,s_p,s,pk,density):
     ##set up networkx grid to calculate neighbors
    ##set up networkx grid to calculate neighbors
     G = grid_graph(dim=[width, length])
 
-    ##set up array gto track mutation states of cells in grid
+    ##set up array to track mutation states of cells in grid
     cells = np.full((length,width),-1)
 
     ##'innoculate' the first row with wild type cells
     cells[0] = 0
 
     ##actual cells per grid cell, as scaled by the MegaPLate experiment,  Baym et. al 
-    cells_per_grid = int(((60*((120/9)*5) * 10**8)/1)/(length*width))
+    cells_per_grid = density
 
     ##mutation odds over mutation space based on the 
     ##expected maximum value for the number (of cells in grid_cell) drawn from a  poisson distruibtion  
     mut_space = np.linspace(0,pk,pk+1)
-    prob_dist = poisson_max_cdf(np.linspace(0,100,101),mut_rate,cells_per_grid)
-
+    #prob_dist = poisson_max_cdf(np.linspace(0,100,101),mut_rate,cells_per_grid)
+    prob_dist = poisson.cdf(np.linspace(0,100,101),mut_rate)
     ##save all cell 
     #cell_history = []
 
     ##indicate that the half time has been reached yet
     half_time =0 
 
-    cell_history= []
-    count = 0 
+    count = 0
+    
     ##when to stop running the cimulation
     #while all(cells[-1] == -1) and len(cell_history) != 40002:
     #while all(cells[-int(ln/g)+2] == -1) and len(cell_history) != 40002:
-
+    
+    #stop running when a cell has made it to the last row
     while all(cells[-1] == -1):
+        if all(cells[3* int(length/divs) -2] != -1 and half_time = 0:
+               half_time = count 
         ##each itteration the  most recent cell state is added to the cell_history array
         cell_history.append(cells.tolist())
-        #count = count + 1
-        ##check if half way mark is reached 
-        if half_time ==0:
-            if all(cells[-3*int(length/divs)+2] == -1) == False:
-                half_time = count
+
 
 
 
         ##find slots where there is a living cells
-        cells_where =  np.where(cells != -1)
+        cells_where =  np.where(cells >= 0)
 
 
         ##create a randomized list of the living cells with which to iterate through
-        cells_list = []
+        live_cells_list = []
         for x, y in zip(cells_where[0], cells_where[1]):
-            cells_list.append([x,y])
+            live_cells_list.append([x,y])
 
-        np.random.shuffle(cells_list)
+        np.random.shuffle(live_cells_list)
+        
+        
+        for j in live_cells_list:
+                
+                        ##assign some amount (including 0) of additional mutations to the parent cell spot
+                        #which now effectively becomes one of the daughter cells
 
-
-        for j in cells_list: 
-            g_draw = 2 * random() -1
-
-            ##death
-            #fitness = fitness_values[fitness_values['mutations']==cells[tuple(j)]][fitness_values['drug']==ab[tuple(j)]]['fitness'].iloc[0]
-            if fitness_func(abx[tuple(j)],cells[tuple(j)],k,s_p,s,pk)< g_draw :
-                cells[tuple(j)] == -1
-            else:
+                    
                 empty_neighbors = [x for x in G.neighbors(tuple(j)) if cells[tuple((x))] ==-1 ] 
-                if empty_neighbors: 
-                    pick = choice(empty_neighbors)
+                #if the array containing the empty neighbors is not empty picj one at random
+                if empty_neighbors:
+                    g_draw = 2 * random() -1
+                    if fitness_func(abx[tuple(j)],cells[tuple(j)],k,s_p,s,pk)> g_draw :
+                        pick = choice(empty_neighbors)
+                        m = mutation(random(),prob_dist)
 
+                        m_net = []
+                        for i in range(m):
+                            m_net.append(choice([-1,1]))
+                            cells[tuple(j)] = cells[tuple(j)]+sum(m_net)
 
-                    m = mutation(random(),prob_dist)
-
-                    if m!=0:
-                        cells[tuple(j)] = cells[tuple(j)]+m
-
-
-                    cells[tuple(pick)] = cells[tuple(j)]
-
-
-        count = count + 1
-    return cell_history, half_time, cells
+                        ##other daughter cells occupies chosen empty spot
+                        cells[tuple(pick)] = cells[tuple(j)]
+                    
+            
+        count = count+1
+    return count,half_time,cells
 
 
 
